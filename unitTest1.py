173a0d25d227eeb0d5d68ff9a2066bda0ec06992
diditgot <150419302+diditgot@users.noreply.github.com>
2023-11-14 12:49:49 -0500

    Updated first unit test

--- unitTest1.py
+++ unitTest1.py
@@ -4,6 +4,6 @@ import unittest
 class TestSum(unittest.TestCase):
     def test_integer(self):
         self.assertEqual(program.add(2, 2), 4)
-
+        
 if __name__ == '__main__':
     unittest.main()
